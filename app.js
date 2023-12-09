165fce4dd2dad0dd90d85b7cba5ca1a6513365d2
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2023-12-09 21:42:24 +0800

    Server Set

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve HTML files from the 'main' directory
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/verification', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main', 'verification.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main', 'login.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main', 'register.html'));
+});
+
+// Serve static files (CSS and images)
+app.use('/main', express.static(path.join(__dirname, 'main')));
+app.use('/Images', express.static(path.join(__dirname, 'Images')));
+
+// Start the server
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
